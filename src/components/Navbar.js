20e3f367d6cf1399fd15e5c4a1e79fe8aa5d84a1
Drew Hayward <d.andrew.hayward@gmail.com>
2020-04-26 12:32:11 -0400

    Add Google Analytics

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import { Container, Col, Row } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const navbar = () =>
