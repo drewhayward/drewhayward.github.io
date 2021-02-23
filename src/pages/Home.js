c0bd89786a4e74ab37297f9562998c3e198e0103
Drew Hayward <d.andrew.hayward@gmail.com>
2021-02-23 11:07:53 -0500

    update profile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,12 +10,12 @@ import profilePic from '../profile500.jpg'
 const Home = () => (
     <Container>
         <Row className="justify-content-center align-items-center" fluid>
-            <Col style={{'text-align':'center', 'padding':10}} md="auto" className="justify-items-center">
-                    <Image src={profilePic} roundedCircle fluid/>
-                    <h1>Drew Hayward</h1>
-                    <p>PhD Student at Michigan State University</p>
-                    <SocialLinks/>
-                    <p>I'm interested in the intersection of learning, language understanding and reasoning. <br/>
+            <Col style={{ 'text-align': 'center', 'padding': 10 }} md="auto" className="justify-items-center">
+                <Image src={profilePic} roundedCircle fluid />
+                <h1>Drew Hayward</h1>
+                <p>Master's Student at Michigan State University</p>
+                <SocialLinks />
+                <p>I'm interested in the intersection of learning, language understanding and reasoning. <br />
                         Currently, I'm exploring the use of neuro-symbolic approaches in Visual Question Answering.</p>
             </Col>
         </Row>
