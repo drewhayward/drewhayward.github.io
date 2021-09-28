5069c37c85a0e41177ed90ba4f8e27a0ee4c2657
Drew Hayward <d.andrew.hayward@gmail.com>
2021-09-28 13:12:41 -0400

    Updates

---
+++ precache-manifest.dbeb5178fa8549a138976baf1a3026dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce56c7b46d7523a5940676fffa817b5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e9cb510a4c37243ed2",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "32aa7a8e18bcdc4b376c",
+    "url": "/static/css/main.fd4d8688.chunk.css"
+  },
+  {
+    "revision": "c7e9cb510a4c37243ed2",
+    "url": "/static/js/2.30340d80.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.30340d80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32aa7a8e18bcdc4b376c",
+    "url": "/static/js/main.906e7440.chunk.js"
+  },
+  {
+    "revision": "4a622ea621e71266c231",
+    "url": "/static/js/runtime-main.2b353301.js"
+  },
+  {
+    "revision": "8e423ba3dbda7c6babd6b456b72826b4",
+    "url": "/static/media/profile500.8e423ba3.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.586fb78ccd102cb1396deb8d76061f6f.js"
+  "/precache-manifest.dbeb5178fa8549a138976baf1a3026dc.js"
 );
 
 self.addEventListener('message', (event) => {
