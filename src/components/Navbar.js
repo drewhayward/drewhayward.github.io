9e2f67be869e92141fa0897d9fd837ebedb11b09
Drew Hayward <d.andrew.hayward@gmail.com>
2021-10-14 22:54:53 -0400

    add TTR

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import { Container } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse'
 
 const navbar = () =>
     (
@@ -10,14 +11,15 @@ const navbar = () =>
         <Container fluid='sm'>
             <Navbar.Brand  as={Link} to="/">Drew Hayward</Navbar.Brand>
             {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-            <Nav.Link href="/drew_hayward_resume.pdf">Resume</Nav.Link>
-            {/* <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ml-auto">
-                <Nav.Link as={Link} to="/">About</Nav.Link>
+                    <Nav.Link href="https://talktoresume.com">Talk To Resume</Nav.Link>
+                    <Nav.Link href="/drew_hayward_resume.pdf">Resume</Nav.Link>
+                {/* <Nav.Link as={Link} to="/">About</Nav.Link>
                 <Nav.Link as={Link} to="/publications">Publications</Nav.Link>
-                <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                <Nav.Link as={Link} to="/projects">Projects</Nav.Link> */}
                 </Nav>
-            </Navbar.Collapse> */}
+            </Navbar.Collapse>
         </Container>
     </Navbar>
     )
