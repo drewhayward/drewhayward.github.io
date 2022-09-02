8f3aef4ef052abe96a1c5ec1e940cdfb542a0f91
Drew Hayward <d.andrew.hayward@gmail.com>
2022-09-02 19:48:16 -0400

    Remove old desc

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,12 +13,8 @@ const Home = () => (
             <Col style={{ 'text-align': 'center', 'padding': 10 }} md="auto" className="justify-items-center">
                 <Image src={profilePic} roundedCircle fluid />
                 <h1>Drew Hayward</h1>
-                <p>Master's Student at Michigan State University</p>
+                <p>Software Engineer</p>
                 <SocialLinks />
-                <p>Hi! I'm a masters student with training in Natural Language Processing and Machine Learning.<br />
-                Currently, I'm looking for positions where I can leverage my experience in an engineering role and grow my engineering skills.<br />
-                If you'd like to chat feel free to send me an email.
-                </p>
             </Col>
         </Row>
     </Container>
