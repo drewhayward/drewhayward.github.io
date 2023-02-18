9e688f82fd8c5a26926fad05d8331afb64c7ee32
Drew Hayward <d.andrew.hayward@gmail.com>
2023-02-18 13:48:11 -0500

    remove old links/resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,7 @@ const navbar = () =>
     <Navbar bg="light" expand="lg">
         <Container fluid='sm'>
             <Navbar.Brand  as={Link} to="/">Drew Hayward</Navbar.Brand>
-            {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link href="https://talktoresume.com">Talk To Resume</Nav.Link>
-                    <Nav.Link href="/drew_hayward_resume.pdf">Resume</Nav.Link>
-                {/* <Nav.Link as={Link} to="/">About</Nav.Link>
-                <Nav.Link as={Link} to="/publications">Publications</Nav.Link>
-                <Nav.Link as={Link} to="/projects">Projects</Nav.Link> */}
-                </Nav>
             </Navbar.Collapse>
         </Container>
     </Navbar>
--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -5,16 +5,13 @@ import { SocialIcon } from 'react-social-icons'
 const SocialLinks = () => (
     <Container >
         <Row style={{justifyContent:'center'}}>
-            <Col xs="2">
+            <Col>
                 <SocialIcon network="github" target="_blank" url="https://github.com/drewhayward"/>
             </Col>
-            <Col xs="2">
+            <Col>
                 <SocialIcon network="linkedin" target="_blank" url="https://www.linkedin.com/in/drew-hayward-b25886133/"/>
             </Col>
-            <Col xs="2">
-                <SocialIcon network="instagram" target="_blank" url="https://instagram.com/drewhayward"/>
-            </Col>
-            <Col xs="2">
+            <Col>
                 <SocialIcon network="email" target="_blank" url="mailto:d.andrew.hayward@gmail.com"/>
             </Col>
         </Row>
